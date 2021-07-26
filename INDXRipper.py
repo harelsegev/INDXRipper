9d5835f0cd8451eaa12504bdcadf944a6b69029c
Harel Segev <segevrl@protonmail.com>
2021-07-26 16:36:40 +0300

    Update INDXRipper.py

--- INDXRipper.py
+++ INDXRipper.py
@@ -3,15 +3,20 @@
     Author: Harel Segev
     05/16/2021
 """
-__version__ = "2.5.1"
+__version__ = "2.5.2"
 
 from ntfs import *
 from indx import *
 import argparse
 from datetime import datetime, timedelta, timezone
+from contextlib import suppress
 
 
 class EmptyNameInFilenameAttribute(ValueError):
+    pass
+
+
+class NoFilenameAttributeInRecord(ValueError):
     pass
 
 
@@ -36,10 +41,8 @@ def get_arguments():
 def get_filename_attributes(mft_cluster, record_header):
     for attribute_header in get_attribute_headers(mft_cluster, record_header):
         if get_attribute_type(attribute_header) == "FILE_NAME":
-            try:
+            with suppress(UnicodeError):
                 yield parse_filename_attribute(get_resident_attribute(mft_cluster, attribute_header))
-            except UnicodeDecodeError:
-                continue
 
 
 def get_parent_reference(filename_attribute):
@@ -48,17 +51,10 @@ def get_parent_reference(filename_attribute):
 
 
 def get_filename_attribute_values(mft_cluster, record_header):
-    parent_index, parent_sequence = 0, 0
-    longest_filename, previous_longest_filename_length = "", -1
     for attribute in get_filename_attributes(mft_cluster, record_header):
-        if attribute["FilenameLengthInCharacters"] > previous_longest_filename_length:
-            parent_index, parent_sequence = get_parent_reference(attribute)
-            longest_filename = attribute["FilenameInUnicode"]
-            previous_longest_filename_length = attribute["FilenameLengthInCharacters"]
-
-    if parent_index == 0:
-        return dict()
-    return {"PARENT_INDEX": parent_index, "PARENT_SEQUENCE": parent_sequence, "FILENAME": longest_filename}
+        parent_index, parent_sequence = get_parent_reference(attribute)
+        filename, namespace = attribute["FilenameInUnicode"], attribute["FilenameNamespace"]
+        yield {"PARENT_REFERENCE": (parent_index, parent_sequence), "FILENAME": filename, "NAMESPACE": namespace}
 
 
 def is_directory_index_allocation(attribute_header, mft_cluster):
@@ -66,22 +62,22 @@ def is_directory_index_allocation(attribute_header, mft_cluster):
     return res and get_attribute_name(mft_cluster, attribute_header) == "$I30"
 
 
-def get_index_allocation_attribute(vbr, raw_image, mft_cluster, record_header):
+def get_index_allocation_attributes(vbr, raw_image, mft_cluster, record_header):
     for attribute_header in get_attribute_headers(mft_cluster, record_header):
         if is_directory_index_allocation(attribute_header, mft_cluster):
-            try:
-                return get_non_resident_attribute(vbr, raw_image, mft_cluster, attribute_header)
-            except EmptyNonResidentAttributeError:
-                continue
+            yield get_non_resident_attribute(vbr, raw_image, mft_cluster, attribute_header)
 
 
 def get_mft_dict_values(vbr, raw_image, mft_cluster, record_header):
+    values = dict({"$FILE_NAME": [], "$INDEX_ALLOCATION": []})
     if is_directory(record_header):
-        values = get_filename_attribute_values(mft_cluster, record_header)
-        values["INDEX_ALLOCATION"] = get_index_allocation_attribute(vbr, raw_image, mft_cluster, record_header)
-        return values
+        with suppress(NoFilenameAttributeInRecord):
+            values["$FILE_NAME"] += get_filename_attribute_values(mft_cluster, record_header)
 
-    return dict()
+        with suppress(EmptyNonResidentAttributeError):
+            values["$INDEX_ALLOCATION"] += get_index_allocation_attributes(vbr, raw_image, mft_cluster, record_header)
+
+    return values
 
 
 def get_mft_records(mft_data, vbr):
@@ -96,6 +92,14 @@ def get_mft_records(mft_data, vbr):
             yield current_record, get_sequence_number(record_header), mft_cluster, record_header
 
 
+def add_to_mft_dict(mft_dict, key, values):
+    if key not in mft_dict:
+        mft_dict[key] = values
+    else:
+        mft_dict[key]["$INDEX_ALLOCATION"] += values["$INDEX_ALLOCATION"]
+        mft_dict[key]["$FILE_NAME"] += values["$FILE_NAME"]
+
+
 def get_mft_dict_helper(raw_image, mft_data, vbr):
     base_records = dict()
     extension_records = dict()
@@ -103,10 +107,10 @@ def get_mft_dict_helper(raw_image, mft_data, vbr):
     for index, sequence, mft_cluster, record_header in get_mft_records(mft_data, vbr):
         values = get_mft_dict_values(vbr, raw_image, mft_cluster, record_header)
         if is_base_record(record_header):
-            base_records[(index, sequence)] = values
+            add_to_mft_dict(base_records, (index, sequence), values)
         else:
             base_reference = get_base_record_reference(record_header)
-            extension_records[base_reference] = values
+            add_to_mft_dict(extension_records, base_reference, values)
 
     return base_records, extension_records
 
@@ -116,31 +120,41 @@ def get_mft_dict(raw_image, mft_data, vbr):
 
     for base_reference in extension_records:
         if base_reference in base_records:
-            base_records[base_reference].update(extension_records[base_reference])
+            add_to_mft_dict(base_records, base_reference, extension_records[base_reference])
 
     return base_records
 
 
-ROOT_KEY = (5, 5)
+NAMESPACE_PRIORITY = {"POSIX": 0, "DOS": 1, "WIN32_DOS": 2, "WIN32": 3}
 
 
-def get_path(mft_dict, key, cache, mount_point):
-    current_key = key
+def get_filename_priority(filename):
+    return NAMESPACE_PRIORITY[filename["NAMESPACE"]]
 
-    res = ""
-    while current_key != ROOT_KEY:
-        if current_key in cache:
-            res = cache[current_key] + res
-            break
 
-        res = "/" + mft_dict[current_key]["FILENAME"] + res
-        current_key = mft_dict[current_key]["PARENT_INDEX"], mft_dict[current_key]["PARENT_SEQUENCE"]
-        if current_key not in mft_dict:
-            res = "/$Orphan" + res
-            break
+def get_first_filename(mft_dict, key):
+    return max(mft_dict[key]["$FILE_NAME"], key=get_filename_priority)
 
-    cache[key] = res
-    return mount_point + cache[key]
+
+path_cache = dict({(5, 5): ""})
+
+
+def get_path_helper(mft_dict, key):
+    if key in path_cache:
+        return path_cache[key]
+    else:
+        if key not in mft_dict:
+            path_cache[key] = "$Orphan"
+
+        else:
+            filename = get_first_filename(mft_dict, key)
+            path_cache[key] = get_path_helper(mft_dict, filename["PARENT_REFERENCE"]) + "/" + filename["FILENAME"]
+
+        return path_cache[key]
+
+
+def get_path(mft_dict, key, mount_point):
+    return mount_point + get_path_helper(mft_dict, key)
 
 
 def to_datetime(filetime):
@@ -155,7 +169,7 @@ def to_iso(filetime):
     return to_datetime(filetime).replace(tzinfo=timezone.utc).isoformat()
 
 
-def get_timestamps(filename_attribute, out_bodyfile):
+def get_timestamps_by_format(filename_attribute, out_bodyfile):
     a_time, c_time = filename_attribute["LastAccessTime"], filename_attribute["LastMftChangeTime"]
     m_time, cr_time = filename_attribute["LastModificationTime"], filename_attribute["CreationTime"]
 
@@ -165,17 +179,21 @@ def get_timestamps(filename_attribute, out_bodyfile):
         return to_iso(a_time), to_iso(c_time), to_iso(m_time), to_iso(cr_time)
 
 
-def concatenate(parent_path, filename_attribute):
-    return f"{parent_path}/{filename_attribute['FilenameInUnicode']}"
-
-
-def get_output_by_format(filename_attribute, parent_path, index, sequence, out_bodyfile):
+def get_full_path(filename_attribute, parent_path):
     if not filename_attribute["FilenameLengthInCharacters"]:
         raise EmptyNameInFilenameAttribute
 
-    full_path = concatenate(parent_path, filename_attribute)
-    size, alloc_size = filename_attribute["RealSize"], filename_attribute["AllocatedSize"]
-    a_time, c_time, m_time, cr_time = get_timestamps(filename_attribute, out_bodyfile)
+    return parent_path + "/" + filename_attribute["FilenameInUnicode"]
+
+
+def get_file_size(filename_attribute):
+    return filename_attribute["RealSize"], filename_attribute["AllocatedSize"]
+
+
+def get_output_by_format(filename_attribute, parent_path, index, sequence, out_bodyfile):
+    full_path = get_full_path(filename_attribute, parent_path)
+    size, alloc_size = get_file_size(filename_attribute)
+    a_time, c_time, m_time, cr_time = get_timestamps_by_format(filename_attribute, out_bodyfile)
 
     if out_bodyfile:
         return f"0|{full_path} ($I30)|{index}|------------|0|0|{size}|{a_time}|{m_time}|{c_time}|{cr_time}\n"
@@ -187,40 +205,31 @@ def get_mft_key(index_entry):
     return index_entry["FILE_REFERENCE"]["FileRecordNumber"], index_entry["FILE_REFERENCE"]["SequenceNumber"]
 
 
+def get_entry_output(mft_dict, index_entry, parent_path, deleted_only, out_bodyfile):
+    mft_key = get_mft_key(index_entry)
+    if not deleted_only or mft_key not in mft_dict:
+        return get_output_by_format(index_entry["FILENAME_ATTRIBUTE"], parent_path, *mft_key, out_bodyfile)
+
+
 def get_record_output(mft_dict, index_entries, parent_path, deleted_only, out_bodyfile):
     lines = list()
     for index_entry in index_entries:
-        mft_key = get_mft_key(index_entry)
-        if deleted_only and mft_key in mft_dict:
-            continue
-        try:
-            line = get_output_by_format(index_entry["FILENAME_ATTRIBUTE"], parent_path, *mft_key, out_bodyfile)
-            lines.append(line)
-        except (OverflowError, EmptyNameInFilenameAttribute):
-            continue
+        with suppress(OverflowError, EmptyNameInFilenameAttribute):
+            if line := get_entry_output(mft_dict, index_entry, parent_path, deleted_only, out_bodyfile):
+                lines.append(line)
 
-    return lines
-
-
-def init_line_list(out_bodyfile):
-    lines = list()
-    if not out_bodyfile:
-        lines.append(f"Path,FileNumber,SequenceNumber,Size,AllocatedSize,CreationTime,ModificationTime,AccessTime,"
-                     f"ChangeTime\n") 
     return lines
 
 
 def get_output_lines(mft_dict, vbr, root_name, out_bodyfile, deleted_only):
-    cache = dict()
-    lines = init_line_list(out_bodyfile)
+    if not out_bodyfile:
+        yield "Path,FileNumber,SequenceNumber,Size,AllocatedSize,CreationTime,ModificationTime,AccessTime,ChangeTime\n"
 
     for key in mft_dict:
-        if mft_dict[key]:
-            if index_allocation := mft_dict[key]["INDEX_ALLOCATION"]:
-                index_entries = find_index_entries(index_allocation, *key, vbr)
-                parent_path = get_path(mft_dict, key, cache, root_name)
-                lines += get_record_output(mft_dict, index_entries, parent_path, deleted_only, out_bodyfile)
-    return lines
+        for index_allocation in mft_dict[key]["$INDEX_ALLOCATION"]:
+            index_entries = find_index_entries(index_allocation, *key, vbr)
+            parent_path = get_path(mft_dict, key, root_name)
+            yield get_record_output(mft_dict, index_entries, parent_path, deleted_only, out_bodyfile)
 
 
 def main():
@@ -231,7 +240,8 @@ def main():
         mft_dict = get_mft_dict(raw_image, mft_data, vbr)
 
         with open(args.outfile, 'at+', encoding='utf-8') as outfile:
-            outfile.writelines(get_output_lines(mft_dict, vbr, args.m, args.bodyfile, args.deleted_only))
+            for lines in get_output_lines(mft_dict, vbr, args.m, args.bodyfile, args.deleted_only):
+                outfile.writelines(lines)
 
 
 if __name__ == '__main__':
