7a227e12b9c7c6602cc51e83bb9fed7ae7e88823
Harel Segev <segevrl@protonmail.com>
2021-09-16 09:50:29 +0300

    Update indx.py

--- indx.py
+++ indx.py
@@ -3,13 +3,11 @@
     Author: Harel Segev
     5/12/2021
 """
-from io import BytesIO
 
-from construct import Struct, Int16ul, Const, Padding, Array, Int32ul, ConstError
+from construct import Struct, Int16ul, Const, Padding, Array, Int32ul, ConstError, Seek
 from ntfs import FILE_REFERENCE, FILENAME_ATTRIBUTE, NUM_OF_FIXUP_BYTES
 import re
 
-NODE_HEADER_OFFSET = 24
 FILENAME_ATTRIBUTE_OFFSET_IN_ENTRY = 16
 
 INDEX_RECORD_HEADER = Struct(
@@ -26,55 +24,48 @@ INDEX_RECORD_HEADER = Struct(
 )
 
 INDEX_ENTRY = Struct(
+    Seek(lambda this: this._.offset),
     "FILE_REFERENCE" / FILE_REFERENCE,
     Padding(8),
     "FILENAME_ATTRIBUTE" / FILENAME_ATTRIBUTE
 )
 
 
-def _get_index_records(raw_index_nonresident_stream, vbr):
-    while current_record := raw_index_nonresident_stream.read(vbr["BytsPerIndx"]):
+def get_indx_records(raw_indx_nonresident_stream, vbr):
+    while current_record := raw_indx_nonresident_stream.read(vbr["BytsPerIndx"]):
         yield current_record
 
 
-def _get_index_record_header(index_record):
+def get_indx_record_header(index_record):
     return INDEX_RECORD_HEADER.parse(index_record)
 
 
-def _get_node_slack_offset(record_header):
-    return NODE_HEADER_OFFSET + record_header["FirstEntryOffset"] + record_header["TotalSizeOfIndexEntries"]
+def apply_fixup(indx_record, record_header, vbr):
+    first_fixup_offset = vbr["BytsPerSec"] - NUM_OF_FIXUP_BYTES
+    for i, usn_offset in enumerate(range(first_fixup_offset, vbr["BytsPerIndx"], vbr["BytsPerSec"])):
+        indx_record[usn_offset:usn_offset + NUM_OF_FIXUP_BYTES] = Int16ul.build(record_header["UpdateSequenceArray"][i])
 
 
-def _apply_fixup(index_record, record_header, vbr):
-    for i, usn_offset in enumerate(range(vbr["BytsPerSec"] - NUM_OF_FIXUP_BYTES, vbr["BytsPerIndx"], vbr["BytsPerSec"])):
-        index_record[usn_offset:usn_offset + NUM_OF_FIXUP_BYTES] = Int16ul.build(record_header["UpdateSequenceArray"][i])
+def find_parent_reference_offsets(indx_record, parent_index, parent_sequence):
+    parent_reference = FILE_REFERENCE.build({'FileRecordNumber': parent_index, 'SequenceNumber': parent_sequence})
+    return (match.start() for match in re.finditer(re.escape(parent_reference), indx_record))
 
 
-def _find_parent_reference_offsets(index_record, parent_mft_record_index, parent_mft_record_sequence):
-    parent_reference = FILE_REFERENCE.build({'FileRecordNumber': parent_mft_record_index,
-                                             'SequenceNumber': parent_mft_record_sequence})
-    return (match.start() for match in re.finditer(re.escape(parent_reference), index_record))
-
-
-def _find_index_entries(index_record, parent_mft_record_index, parent_mft_record_sequence):
-    parent_reference_offsets = _find_parent_reference_offsets(index_record, parent_mft_record_index,
-                                                              parent_mft_record_sequence)
-
-    index_record = BytesIO(index_record)
+def get_indx_entries(indx_record, parent_index, parent_sequence):
+    parent_reference_offsets = find_parent_reference_offsets(indx_record, parent_index, parent_sequence)
     for offset in parent_reference_offsets:
-        if (entry_offset := offset - FILENAME_ATTRIBUTE_OFFSET_IN_ENTRY) >= 0:
-            index_record.seek(entry_offset)
-            yield INDEX_ENTRY.parse_stream(index_record)
+        entry_offset = offset - FILENAME_ATTRIBUTE_OFFSET_IN_ENTRY
+        if entry_offset >= 0:
+            yield INDEX_ENTRY.parse(indx_record, offset=entry_offset)
 
 
-def find_index_entries(raw_index_nonresident_stream, parent_mft_record_index, parent_mft_record_sequence, vbr):
-    for index_record in _get_index_records(raw_index_nonresident_stream, vbr):
+def find_index_entries(raw_index_nonresident_stream, parent_index, parent_sequence, vbr):
+    for index_record in get_indx_records(raw_index_nonresident_stream, vbr):
         try:
-            record_header = _get_index_record_header(index_record)
+            record_header = get_indx_record_header(index_record)
         except ConstError:
             continue
 
-        _apply_fixup(index_record, record_header, vbr)
-
-        for entry in _find_index_entries(index_record, parent_mft_record_index, parent_mft_record_sequence):
+        apply_fixup(index_record, record_header, vbr)
+        for entry in get_indx_entries(index_record, parent_index, parent_sequence):
             yield entry
