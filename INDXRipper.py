2c941ca69fc2154732077a06e813c6aedb15aeb8
Harel Segev <segevrl@protonmail.com>
2021-11-05 18:43:42 +0200

    Update INDXRipper.py

--- INDXRipper.py
+++ INDXRipper.py
@@ -3,7 +3,7 @@
     Author: Harel Segev
     05/16/2021
 """
-__version__ = "3.0.1"
+__version__ = "4.0.0"
 
 import argparse
 from sys import stderr
@@ -25,7 +25,7 @@ class NoFilenameAttributeInRecordError(ValueError):
 
 def get_arguments():
     parser = argparse.ArgumentParser(prog="INDXRipper",
-                                     description="find index entries in $INDEX_ALLOCATION attributes")
+                                     description="carve file metadata from NTFS $INDEX_ALLOCATION attributes")
 
     parser.add_argument("image", metavar="image", help="image file path")
     parser.add_argument("outfile", metavar="outfile", help="output file path")
@@ -37,10 +37,7 @@ def get_arguments():
     parser.add_argument("-o", metavar="OFFSET", type=int, default=0, help="offset to an NTFS partition (in sectors)")
     parser.add_argument("-b", metavar="SECTOR_SIZE", type=int, default=512, help="sector size in bytes. default is 512")
     parser.add_argument("-w", choices=["csv", "bodyfile"], default="csv", help="output format. default is csv")
-
-    parser.add_argument("--invalid-only", action="store_true",
-                        help="only display entries with an invalid file reference")
-
+    parser.add_argument("--invalid-only", action="store_true", help="only display invalid entries")
     parser.add_argument("--dedup", action="store_true", help="deduplicate output lines")
     return parser.parse_args()
 
@@ -66,20 +63,24 @@ def is_directory_index_allocation(attribute_header):
     return get_attribute_type(attribute_header) == "INDEX_ALLOCATION" and get_attribute_name(attribute_header) == "$I30"
 
 
-def add_to_mft_values(vbr, raw_image, mft_chunk, attribute_header, values):
+def add_to_mft_values(vbr, raw_image, mft_chunk, attribute_header, record_header, values):
     if get_attribute_type(attribute_header) == "FILE_NAME":
-        values["$FILE_NAME"].append(get_filename_attribute(mft_chunk, attribute_header))
+        filename = get_filename_attribute(mft_chunk, attribute_header)
+        values["PARENT_REFERENCE"].append(get_parent_reference(filename))
+
+        if is_directory(record_header):
+            name_values = {"FILENAME": filename["FilenameInUnicode"], "NAMESPACE": filename["FilenameNamespace"]}
+            values["$FILE_NAME"].append(name_values)
 
     elif is_directory_index_allocation(attribute_header):
         values["$INDEX_ALLOCATION"].append(get_non_resident_attribute(vbr, raw_image, mft_chunk, attribute_header))
 
 
 def get_mft_dict_values(vbr, raw_image, mft_chunk, record_header):
-    values = {"$FILE_NAME": [], "$INDEX_ALLOCATION": []}
-    if is_directory(record_header):
-        for attribute_header in get_attribute_headers(mft_chunk, record_header):
-            with suppress(EmptyNonResidentAttributeError):
-                add_to_mft_values(vbr, raw_image, mft_chunk, attribute_header, values)
+    values = {"$FILE_NAME": [], "$INDEX_ALLOCATION": [], "PARENT_REFERENCE": []}
+    for attribute_header in get_attribute_headers(mft_chunk, record_header):
+        with suppress(EmptyNonResidentAttributeError):
+            add_to_mft_values(vbr, raw_image, mft_chunk, attribute_header, record_header, values)
 
     return values
 
@@ -107,6 +108,7 @@ def add_to_mft_dict(mft_dict, key, values, base_record):
     else:
         mft_dict[key]["$INDEX_ALLOCATION"] += values["$INDEX_ALLOCATION"]
         mft_dict[key]["$FILE_NAME"] += values["$FILE_NAME"]
+        mft_dict[key]["PARENT_REFERENCE"] += values["PARENT_REFERENCE"]
         mft_dict[key]["HAS_BASE_RECORD"] = mft_dict[key]["HAS_BASE_RECORD"] or base_record
 
 
@@ -123,16 +125,23 @@ def get_mft_dict(raw_image, mft_data, vbr):
     return mft_dict
 
 
-NAMESPACE_PRIORITY = {"DOS": 0, "POSIX": 1, "WIN32_DOS": 2, "WIN32": 3}
+NAMESPACE_PRIORITY = {"DOS": 0, "WIN32_DOS": 1, "POSIX": 2, "WIN32": 3}
 
 
 def get_filename_priority(filename):
-    return NAMESPACE_PRIORITY[filename["FilenameNamespace"]]
+    return NAMESPACE_PRIORITY[filename["NAMESPACE"]]
 
 
 def get_first_filename(mft_dict, key):
     if mft_dict[key]["$FILE_NAME"]:
         return max(mft_dict[key]["$FILE_NAME"], key=get_filename_priority)
+    else:
+        raise NoFilenameAttributeInRecordError
+
+
+def get_first_parent_reference(mft_dict, key):
+    if mft_dict[key]["PARENT_REFERENCE"]:
+        return mft_dict[key]["PARENT_REFERENCE"][0]
     else:
         raise NoFilenameAttributeInRecordError
 
@@ -150,8 +159,8 @@ def get_path_helper(mft_dict, key):
     else:
         try:
             filename = get_first_filename(mft_dict, key)
-            parent_reference = get_parent_reference(filename)
-            path_cache[key] = get_path_helper(mft_dict, parent_reference) + "/" + filename["FilenameInUnicode"]
+            parent_reference = get_first_parent_reference(mft_dict, key)
+            path_cache[key] = get_path_helper(mft_dict, parent_reference) + "/" + filename["FILENAME"]
 
         except NoFilenameAttributeInRecordError:
             path_cache[key] = "/$NoName/[FileNumber: {}, SequenceNumber: {}]".format(*key)
@@ -187,9 +196,13 @@ COMMON_FIELDS = {
 OUTPUT_FORMATS = {
     "csv":
     {
-        "fmt": "\"{full_path}\",{flags},{index},{sequence},{size},{alloc_size},{cr_time},{m_time},{a_time},{c_time}\n",
-        "header": "Path,Flags,FileNumber,SequenceNumber,Size,AllocatedSize,CreationTime,ModificationTime,AccessTime,"
-                  "ChangeTime\n",
+        "fmt": "\"{full_path}\",{flags},{comment},{index},{sequence},{size},{alloc_size},{cr_time},{m_time},{a_time},"
+               "{c_time}\n",
+
+        "header": "Path,Flags,Comment,FileNumber,SequenceNumber,Size,AllocatedSize,CreationTime,ModificationTime,"
+                  "AccessTime,ChangeTime\n",
+
+        "comment_fmt": lambda comment: comment,
 
         "fields":
         {
@@ -205,8 +218,11 @@ OUTPUT_FORMATS = {
 
     "bodyfile":
     {
-        "fmt": "0|{full_path} ($I30)|{index}|{mode_prt1}{mode_prt2}|0|0|{size}|{a_time}|{m_time}|{c_time}|{cr_time}\n",
+        "fmt": "0|{full_path} ($I30){comment}|{index}|{mode_prt1}{mode_prt2}|0|0|{size}|{a_time}|{m_time}|"
+               "{c_time}|{cr_time}\n",
+
         "header": "",
+        "comment_fmt": lambda comment: f" ({comment})" if comment else "",
 
         "fields":
         {
@@ -220,7 +236,7 @@ OUTPUT_FORMATS = {
 
         } | COMMON_FIELDS,
 
-        "adapted_fields": {"cr_time": to_epoch, "m_time": to_epoch, "a_time": to_epoch, "c_time": to_epoch},
+        "adapted_fields": {"cr_time": to_epoch, "m_time": to_epoch, "a_time": to_epoch, "c_time": to_epoch}
     }
 }
 
@@ -236,9 +252,10 @@ def populate_fmt_dict(fmt_dict, index_entry, output_format):
             fmt_dict[field] = adapted_fields[field](fmt_dict[field])
 
 
-def get_entry_output(index_entry, parent_path, output_format):
+def get_entry_output(index_entry, parent_path, output_format, comment):
     fmt_dict = {
-        "full_path": parent_path + "/" + index_entry["FilenameInUnicode"]
+        "full_path": parent_path + "/" + index_entry["FilenameInUnicode"],
+        "comment": OUTPUT_FORMATS[output_format]["comment_fmt"](comment)
     }
 
     populate_fmt_dict(fmt_dict, index_entry, output_format)
@@ -256,14 +273,23 @@ def get_collection(dedup):
         return list(), list.append
 
 
-def get_record_output(mft_dict, index_entries, parent_path, invalid_only, dedup, output_format):
+def why_is_entry_invalid(mft_dict, mft_key, parent_key):
+    if mft_key in mft_dict and mft_dict[mft_key]["HAS_BASE_RECORD"]:
+        if parent_key not in mft_dict[mft_key]["PARENT_REFERENCE"]:
+            return "old path"
+        return ""  # entry is valid
+    else:
+        return "invalid file reference"
+
+
+def get_record_output(mft_dict, index_entries, parent_path, parent_key, invalid_only, dedup, output_format):
     lines, add_line = get_collection(dedup)
 
     for index_entry in index_entries:
         mft_key = get_mft_key(index_entry)
 
-        if not invalid_only or mft_key not in mft_dict or not mft_dict[mft_key]["HAS_BASE_RECORD"]:
-            line = get_entry_output(index_entry, parent_path, output_format)
+        if (reason_is_invalid := why_is_entry_invalid(mft_dict, mft_key, parent_key)) or not invalid_only:
+            line = get_entry_output(index_entry, parent_path, output_format, reason_is_invalid)
             add_line(lines, line)
 
     return lines
@@ -276,7 +302,8 @@ def get_output_lines(mft_dict, vbr, root_name, invalid_only, dedup, output_forma
         for index_allocation in mft_dict[key]["$INDEX_ALLOCATION"]:
             index_entries = find_index_entries(index_allocation, *key, vbr)
             parent_path = get_path(mft_dict, key, root_name)
-            yield get_record_output(mft_dict, index_entries, parent_path, invalid_only, dedup, output_format)
+
+            yield get_record_output(mft_dict, index_entries, parent_path, key, invalid_only, dedup, output_format)
 
 
 def main():
