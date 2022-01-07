8c147fe818ca4ebfd1b605a624487f9a42f9734c
Harel Segev <segevrl@protonmail.com>
2022-01-07 11:00:52 +0200

    Update indx.py

--- indx.py
+++ indx.py
@@ -3,51 +3,43 @@
     Author: Harel Segev
     5/12/2021
 """
+
+from construct import Struct, Const, Padding, Array, Seek, Optional, StopIf, FlagsEnum, Enum
+from construct import PaddedString, Int8ul, Int16ul, Int32ul, Int64ul
+from construct import Check, CheckError, StreamError
+
 from contextlib import suppress
-from datetime import datetime, timedelta
 from io import BytesIO
 import re
 
-from construct import Struct, Const, Padding, Array, Seek, Optional, StopIf, FlagsEnum, Enum, PaddedString, Adapter
-from construct import Int8ul, Int16ul, Int32ul, Int64ul
-from construct import Check, CheckError, StreamError
+from ntfs import FILE_REFERENCE
 
-from ntfs import FILE_REFERENCE, NUM_OF_FIXUP_BYTES
-
-
-INDX_RECORD_HEADER = Struct(
+INDEX_RECORD_HEADER = Struct(
     "Magic" / Optional(Const(b'INDX')),
     StopIf(lambda this: this.Magic is None),
 
     "UpdateSequenceOffset" / Int16ul,
     "UpdateSequenceSize" / Int16ul,
-    Seek(lambda this: this.UpdateSequenceOffset),
 
+    Padding(20),
+    "EndOfEntriesOffset" / Int32ul,
+
+    Seek(lambda this: this.UpdateSequenceOffset),
     "UpdateSequence" / Int16ul,
     "UpdateSequenceArray" / Array(lambda this: this.UpdateSequenceSize - 1, Int16ul)
 )
 
 
-class FiletimeAdapter(Adapter):
-    def _decode(self, obj, context, path):
-        return datetime(1601, 1, 1) + timedelta(microseconds=(obj / 10))
-
-    def _encode(self, obj, context, path):
-        delta = obj - datetime(1601, 1, 1)
-        return 10 ** 7 * delta.total_seconds()
-
-
-Filetime = FiletimeAdapter(Int64ul)
-
-
 INDEX_ENTRY = Struct(
     "FILE_REFERENCE" / FILE_REFERENCE,
-    Padding(16),
+    "IndexEntrySize" / Int16ul,
+    Padding(14),
 
-    "CreationTime" / Filetime,
-    "LastModificationTime" / Filetime,
-    "LastMftChangeTime" / Filetime,
-    "LastAccessTime" / Filetime,
+    "CreationTime" / Int64ul,
+    "LastModificationTime" / Int64ul,
+    "LastMftChangeTime" / Int64ul,
+    "LastAccessTime" / Int64ul,
+
     "AllocatedSize" / Int64ul,
     "RealSize" / Int64ul,
     "Flags" / FlagsEnum(Int32ul,
@@ -55,6 +47,7 @@ INDEX_ENTRY = Struct(
                         HIDDEN=0x0002,
                         SYSTEM=0x0004,
                         ARCHIVE=0x0020,
+                        DEVICE=0x0040,
                         NORMAL=0x0080,
                         TEMPORARY=0x0100,
                         SPARSE=0x0200,
@@ -75,52 +68,118 @@ INDEX_ENTRY = Struct(
 )
 
 
-def get_indx_records(raw_indx_nonresident_stream, vbr):
-    while current_record := raw_indx_nonresident_stream.read(vbr["BytsPerIndx"]):
+def get_index_records_helper(index_allocation_attribute, vbr):
+    while current_record := index_allocation_attribute.read(vbr["BytsPerIndx"]):
         yield current_record
 
 
-def get_indx_record_header(indx_record):
-    return INDX_RECORD_HEADER.parse(indx_record)
+def get_index_record_header(index_record):
+    return INDEX_RECORD_HEADER.parse(index_record)
 
 
-def is_valid_indx_record(record_header):
+def is_valid_index_record(record_header):
     return record_header["Magic"] is not None
 
 
-def apply_fixup(indx_record, record_header, vbr):
-    first_fixup_offset = vbr["BytsPerSec"] - NUM_OF_FIXUP_BYTES
-    for i, usn_offset in enumerate(range(first_fixup_offset, vbr["BytsPerIndx"], vbr["BytsPerSec"])):
-        indx_record[usn_offset:usn_offset + NUM_OF_FIXUP_BYTES] = Int16ul.build(record_header["UpdateSequenceArray"][i])
+NODE_HEADER_OFFSET_IN_RECORD = 24
 
 
-def find_parent_reference_offsets(indx_record, parent_index, parent_sequence):
-    parent_reference = FILE_REFERENCE.build({'FileRecordNumber': parent_index, 'SequenceNumber': parent_sequence})
-    return [match.start() for match in re.finditer(re.escape(parent_reference), indx_record)]
+def get_slack_offset(record_header):
+    return record_header["EndOfEntriesOffset"] + NODE_HEADER_OFFSET_IN_RECORD
+
+
+def apply_fixup(index_record, record_header, vbr):
+    for i, usn_offset in enumerate(range(vbr["BytsPerSec"] - 2, vbr["BytsPerIndx"], vbr["BytsPerSec"])):
+        index_record[usn_offset:usn_offset + 2] = Int16ul.build(record_header["UpdateSequenceArray"][i])
+
+
+def get_index_records(index_allocation_attribute, vbr):
+    for index_record in get_index_records_helper(index_allocation_attribute, vbr):
+        record_header = get_index_record_header(index_record)
+
+        if is_valid_index_record(record_header):
+            apply_fixup(index_record, record_header, vbr)
+
+            yield index_record, record_header
+
+
+def get_parent_reference_offsets(index_record, parent_reference):
+    parent_index, parent_sequence = parent_reference
+
+    magic = FILE_REFERENCE.build({
+        "FileRecordNumber": parent_index,
+        "SequenceNumber": parent_sequence
+    })
+
+    return [match.start() for match in re.finditer(re.escape(magic), index_record)]
 
 
 FILENAME_ATTRIBUTE_OFFSET_IN_ENTRY = 16
 
 
-def get_index_entries(indx_record, parent_index, parent_sequence):
-    indx_record_stream = BytesIO(indx_record)
+def get_entries_in_record(index_record, parent_reference):
+    index_record_stream = BytesIO(index_record)
 
-    for offset in find_parent_reference_offsets(indx_record, parent_index, parent_sequence):
+    for offset in get_parent_reference_offsets(index_record, parent_reference):
         entry_offset = offset - FILENAME_ATTRIBUTE_OFFSET_IN_ENTRY
 
         if entry_offset >= 0:
-            indx_record_stream.seek(entry_offset)
+            index_record_stream.seek(entry_offset)
 
-            with suppress(StreamError, OverflowError, CheckError, UnicodeDecodeError):
-                yield INDEX_ENTRY.parse_stream(indx_record_stream)
+            with suppress(StreamError, CheckError, OverflowError, UnicodeDecodeError):
+                yield INDEX_ENTRY.parse_stream(index_record_stream), entry_offset
 
 
-def find_index_entries(raw_index_nonresident_stream, parent_index, parent_sequence, vbr):
-    for indx_record in get_indx_records(raw_index_nonresident_stream, vbr):
-        record_header = get_indx_record_header(indx_record)
+def get_entry_size(index_entry):
+    return index_entry["IndexEntrySize"]
 
-        if is_valid_indx_record(record_header):
-            apply_fixup(indx_record, record_header, vbr)
 
-            for entry in get_index_entries(indx_record, parent_index, parent_sequence):
+def get_all_entries_in_attribute(index_allocation_attribute, parent_reference, vbr):
+    for index_record, record_header in get_index_records(index_allocation_attribute, vbr):
+        slack_offset = get_slack_offset(record_header)
+
+        for entry, entry_offset in get_entries_in_record(index_record, parent_reference):
+            entry["IsSlack"] = entry_offset + get_entry_size(entry) >= slack_offset
+            yield entry
+
+
+def get_slack_entries_in_attribute_helper(index_allocation_attribute, parent_reference, vbr):
+    allocated_entries, slack_entries = {}, []
+
+    for entry in get_all_entries_in_attribute(index_allocation_attribute, parent_reference, vbr):
+        if entry["IsSlack"]:
+            slack_entries.append(entry)
+        else:
+            entry_filename = entry["FilenameInUnicode"]
+            allocated_entries[entry_filename] = get_file_reference(entry)
+
+    return allocated_entries, slack_entries
+
+
+def get_file_reference(entry):
+    return entry["FILE_REFERENCE"]["FileRecordNumber"], entry["FILE_REFERENCE"]["SequenceNumber"]
+
+
+def get_slack_entries_in_attribute(index_allocation_attribute, parent_reference, vbr):
+    allocated_entries, slack_entries = get_slack_entries_in_attribute_helper(
+        index_allocation_attribute,
+        parent_reference,
+        vbr
+    )
+
+    for entry in slack_entries:
+        filename = entry["FilenameInUnicode"]
+
+        if filename in allocated_entries:
+            if not get_file_reference(entry) == allocated_entries[filename]:
                 yield entry
+
+        else:
+            yield entry
+
+
+def get_entries_in_attribute(index_allocation_attribute, parent_reference, slack_only, vbr):
+    if slack_only:
+        return get_slack_entries_in_attribute(index_allocation_attribute, parent_reference, vbr)
+    else:
+        return get_all_entries_in_attribute(index_allocation_attribute, parent_reference, vbr)
